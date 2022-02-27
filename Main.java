103f3417824f14ebb1555a38fb26dc0cde9d7506
haileyhjyun <96277611+haileyhjyun@users.noreply.github.com>
2022-02-26 23:20:36 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main{
+
+	public static int max(int a, int b) {
+		return (a>b)? a:b;
+	}
+	public static void main(String[]args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("배열의 크기: ");
+		int number = scanner.nextInt();
+		int [] array = new int[number];
+		
+		for(int i=0; i<number; i++) {
+			System.out.println("배열에 정수를 하나씩 입력하세요: ");
+			array[i] = scanner.nextInt();
+		}
+		int result = -1;
+		for(int i=0; i<number; i++) {
+			result = max(result, array[i]);
+		}
+		System.out.println("최댓값은 "+ result);
+	}
+}
